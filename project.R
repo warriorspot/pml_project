bbd0ff2ac88eb341b5d2b96389d22580a2ac624c
Brennan Cleveland <warriorspot@gmail.com>
2016-03-01 07:57:38 +0000

    Add cross validation

--- project.R
+++ project.R
@@ -1,20 +1,37 @@
 
+
+if(file.exists("pml-training.csv") == FALSE) {
+    download.file("https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv",
+                  destfile="pml-training.csv")
+}
+
+if(file.exists("pml-testing.csv") == FALSE) {
+    download.file("https://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv",
+                  destfile="pml-testing.csv")
+
+}
+
 library(caret)
 
 set.seed(1234)
 
-data <- read.csv("./Documents/pml-training.csv", stringsAsFactors=FALSE)
+data <- read.csv("./pml-training.csv", stringsAsFactors=FALSE)
 
-inTrain <- with(data, createDataPartition(y=classe, p=0.7, list=FALSE))
-training <- data[inTrain,]
-testing <- data[-inTrain,]
+# Prepare the data for prediction
 
-# Consider only numeric predictors
-classe <- training$classe
-tclean <- training[, sapply(training, is.numeric)] 
+#Get rid of the first four columns, which dont contain measurement data
+tclean <- subset(data, select=seq(5, ncol(data)))
+
+# Consider only numeric predictors with no NA values
+classe <- data$classe
+tclean <- data[, sapply(data, is.numeric)] 
 tclean <- tclean[, colSums(is.na(tclean)) == 0] 
 tclean$classe <- classe
 
-#Train a random forest, preprocessing with PCA
-m <- train(classe ~ ., method="rf", preProcess=c("pca"), data=tclean)
+#Set up k-fold cross validation
+tControl <- trainControl(method="cv", number=10)
+
+#Train a naive Bayes model, preprocessing with PCA, using K-Fold cross validation
+m <- train(classe ~ ., method="nb", preProcess=c("pca"), trainControl=tControl, data=tclean)
+
 
